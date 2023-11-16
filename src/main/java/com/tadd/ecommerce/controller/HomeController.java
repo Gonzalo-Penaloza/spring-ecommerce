b930ffbb6cc716571c772ef66a320f34af1d98aa
Gonzalo Penaloza <gonzape997@gmail.com>
2023-11-16 00:47:04 -0300

    Agregado de vista home para usuario normal

---
+++ src/main/java/com/tadd/ecommerce/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.tadd.ecommerce.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tadd.ecommerce.model.Producto;
+import com.tadd.ecommerce.service.ProductoService;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@Autowired
+	private ProductoService productoService;
+
+	@GetMapping("")
+	public String home(ModelMap model) {
+
+		List<Producto> productos = productoService.findAll();
+
+		model.addAttribute("productos", productos);
+
+		return "usuario/home";
+	}
+
+}
